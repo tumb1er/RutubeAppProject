4fc1634a70937f10b838db2bff3e6bf5ee974a95
tumb1er <zimbler@gmail.com>
2013-07-07 17:37:59 +0400

    Модели User и Video не требуют при создании объекта Context, сделан парсинг JSON.

--- RutubeAPI/src/ru/rutube/RutubeAPI/models/User.java
+++ RutubeAPI/src/ru/rutube/RutubeAPI/models/User.java
@@ -30,88 +30,85 @@ public class User {
     private static final String LOG_TAG = User.class.getName();
     private static final String EMAIL = "email";
     private static final String AUTH_COOKIE = "cookie";
-    private Context mContext;
-    private RequestListener mRequestListener;
     private String mToken;
 
 
-    public User(Context context, RequestListener listener) {
-        this.mContext = context;
-        this.mRequestListener = listener;
-        init();
+    public User(String token) {
+        mToken = token;
+    }
+    public User() {
+        this(null);
     }
 
-    public User(Context context) {
-        this(context, (RequestListener)context);
-        init();
-    }
-
-    Response.Listener<JSONObject> getTokenListener = new Response.Listener<JSONObject>() {
-        @Override
-        public void onResponse(JSONObject response) {
-            try {
-                String token = parseToken(response);
-                Bundle bundle = new Bundle();
-                bundle.putString(Constants.Result.TOKEN, token);
-                saveToken(token);
-                mRequestListener.onResult(TOKEN_RESULT, bundle);
-            } catch (JSONException e) {
-                RequestListener.RequestError error = new RequestListener.RequestError(e.getMessage());
-                mRequestListener.onRequestError(TOKEN_RESULT, error);
+    Response.Listener<JSONObject> getTokenListener(final RequestListener requestListener) {
+        return new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    String token = parseToken(response);
+                    Bundle bundle = new Bundle();
+                    bundle.putString(Constants.Result.TOKEN, token);
+                    requestListener.onResult(TOKEN_RESULT, bundle);
+                } catch (JSONException e) {
+                    RequestListener.RequestError error = new RequestListener.RequestError(e.getMessage());
+                    requestListener.onRequestError(TOKEN_RESULT, error);
+                }
             }
-        }
-    };
-    Response.Listener<JSONObject> loginListener = new Response.Listener<JSONObject>() {
-        @Override
-        public void onResponse(JSONObject response) {
-            Log.d(LOG_TAG, "onLoginResponse");
-            Bundle bundle = new Bundle();
-            mRequestListener.onResult(LOGIN_RESULT, bundle);
-        }
-    };
-    Response.ErrorListener vollerErrorListener = new Response.ErrorListener() {
-        @Override
-        public void onErrorResponse(VolleyError error) {
-            mRequestListener.onVolleyError(error);
-        }
-    };
-
-    private void init() {
-        mToken = getToken();
+        };
+    }
+    Response.Listener<JSONObject> getLoginListener(final RequestListener requestListener)
+    {
+        return new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(LOG_TAG, "onLoginResponse");
+                Bundle bundle = new Bundle();
+                requestListener.onResult(LOGIN_RESULT, bundle);
+            }
+        };
+    }
+    Response.ErrorListener getErrorListener(final RequestListener requestListener)
+    {
+        return new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                requestListener.onVolleyError(error);
+            }
+        };
     }
 
-    public void saveCookie(String cookie) {
-        SharedPreferences prefs = mContext.getSharedPreferences(USER_DETAILS, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putString(AUTH_COOKIE, cookie);
-        editor.commit();
-    }
+//    public void saveCookie(String cookie) {
+//        SharedPreferences prefs = mContext.getSharedPreferences(USER_DETAILS, Context.MODE_PRIVATE);
+//        SharedPreferences.Editor editor = prefs.edit();
+//        editor.putString(AUTH_COOKIE, cookie);
+//        editor.commit();
+//    }
 
     protected String parseToken(JSONObject data) throws JSONException {
         Log.d(LOG_TAG, "Result: " + data.toString());
         return data.getString(TOKEN);
     }
 
-    protected void saveToken(String token) {
-        SharedPreferences prefs = mContext.getSharedPreferences(USER_DETAILS, Context.MODE_PRIVATE);
+    public static void saveToken(Context context, String token) {
+        SharedPreferences prefs = context.getSharedPreferences(USER_DETAILS, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
         editor.putString(TOKEN, token);
         editor.commit();
-        mToken = token;
     }
 
-    protected String getToken() {
-        SharedPreferences prefs = mContext.getSharedPreferences(USER_DETAILS, Context.MODE_PRIVATE);
+    public static String getToken(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences(USER_DETAILS, Context.MODE_PRIVATE);
         return prefs.getString(TOKEN, null);
     }
 
-    public String getAuthCookie() {
-        SharedPreferences prefs = mContext.getSharedPreferences(USER_DETAILS, Context.MODE_PRIVATE);
-        return prefs.getString(AUTH_COOKIE, null);
-    }
+//    public String getAuthCookie() {
+//        SharedPreferences prefs = mContext.getSharedPreferences(USER_DETAILS, Context.MODE_PRIVATE);
+//        return prefs.getString(AUTH_COOKIE, null);
+//    }
 
-    public JsonObjectRequest getTokenRequest(String email, String password) {
-        String loginUri = RutubeAPI.getUrl(mContext, R.string.token_uri);
+    public JsonObjectRequest getTokenRequest(String email, String password, Context context,
+                                             RequestListener requestListener) {
+        String loginUri = RutubeAPI.getUrl(context, R.string.token_uri);
         JSONObject requestData = new JSONObject();
         try {
             requestData.put(USERNAME, email);
@@ -120,25 +117,26 @@ public class User {
             e.printStackTrace();
         }
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, loginUri,
-                requestData, getTokenListener, vollerErrorListener);
+                requestData, getTokenListener(requestListener),
+                getErrorListener(requestListener));
         request.setShouldCache(true);
         return request;
     }
 
-    public JsonObjectRequest getLoginRequest(String email, String password) {
-        String loginUri = RutubeAPI.getUrl(mContext, R.string.login_uri);
-        JSONObject requestData = new JSONObject();
-        try {
-            requestData.put(EMAIL, email);
-            requestData.put(PASSWORD, password);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, loginUri,
-                requestData, loginListener, vollerErrorListener);
-        request.setShouldCache(false);
-        return request;
-    }
+//    public JsonObjectRequest getLoginRequest(String email, String password) {
+//        String loginUri = RutubeAPI.getUrl(mContext, R.string.login_uri);
+//        JSONObject requestData = new JSONObject();
+//        try {
+//            requestData.put(EMAIL, email);
+//            requestData.put(PASSWORD, password);
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, loginUri,
+//                requestData, loginListener, vollerErrorListener);
+//        request.setShouldCache(false);
+//        return request;
+//    }
 
     public boolean isAuthenticated() {
        return (mToken != null);
--- RutubeAPI/src/ru/rutube/RutubeAPI/models/Video.java
+++ RutubeAPI/src/ru/rutube/RutubeAPI/models/Video.java
@@ -1,16 +1,20 @@
 package ru.rutube.RutubeAPI.models;
 
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import ru.rutube.RutubeAPI.R;
 import ru.rutube.RutubeAPI.RutubeAPI;
@@ -20,59 +24,121 @@ import ru.rutube.RutubeAPI.requests.RequestListener;
  * Created by tumbler on 16.06.13.
  */
 public class Video {
-    public static final int VIDEO_RESULT = 1;
+    public final static SimpleDateFormat dtf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+    public static final int VIDEO_RESULT = 1; // TODO: GET /api/video/<id>/
     public static final int TRACK_INFO_RESUILT = 2;
 
     private static final String LOG_TAG = Video.class.getName();
-    private Context mContext;
-    private RequestListener mRequestListener;
+    private static final String JSON_CREATED = "created_ts";
+    private static final String JSON_AUTHOR = "author";
+    private static final String JSON_TITLE = "title";
+    private static final String JSON_DESCIPTION = "description";
+    private static final String JSON_THUMBNAIL_URL = "thumbnail_url";
+    private static final String JSON_VIDEO_ID = "id";
     private String mVideoId;
+    private String mTitle;
+    private String mDescription;
+    private Date mCreated;
+    private Uri mThumbnailUri;
+    private Author mAuthor;
 
 
-    public Video(String videoId, Context context, RequestListener listener) {
+    public Video(String videoId) {
+        this(videoId, null, null, null, null, null);
+    }
+
+    protected Video(String videoId, String title, String description, Date created, Uri thumbnailUri, Author author){
         this.mVideoId = videoId;
-        this.mContext = context;
-        this.mRequestListener = listener;
+        this.mTitle = title;
+        this.mDescription = description;
+        this.mCreated = created;
+        this.mThumbnailUri = thumbnailUri;
+        this.mAuthor = author;
+    }
+    private static Date parseDate(String data) {
+        try {
+            return dtf.parse(data);
+        } catch (ParseException e) {
+            return new Date(0);
+        }
     }
 
-    public Video(String videoId, Context context) {
-        this(videoId, context, (RequestListener) context);
+    public static Video fromJSON(JSONObject data) throws JSONException {
+        Date created = parseDate(data.getString(JSON_CREATED));
+        JSONObject author_json = data.optJSONObject(JSON_AUTHOR);
+        Author author = null;
+        if (author_json != null)
+            author = Author.fromJSON(author_json);
+        String title = data.getString(JSON_TITLE);
+        String description = data.getString(JSON_DESCIPTION);
+        Uri thumbnailUri = Uri.parse(data.getString(JSON_THUMBNAIL_URL));
+        String videoId = data.getString(JSON_VIDEO_ID);
+        Log.d(FeedItem.class.getName(), "Created item: " + videoId + " " + String.valueOf(created));
+        return new Video(videoId, title, description, created, thumbnailUri, author);
     }
 
-    Response.Listener<JSONObject> getTrackInfoListener = new Response.Listener<JSONObject>() {
-        @Override
-        public void onResponse(JSONObject response) {
-            try {
-                Trackinfo ti = parseTrackInfo(response);
-                Bundle bundle = new Bundle();
-                bundle.putParcelable(Constants.Result.TRACKINFO, ti);
-                mRequestListener.onResult(TRACK_INFO_RESUILT, bundle);
-            } catch (JSONException e) {
-                RequestListener.RequestError error = new RequestListener.RequestError(e.getMessage());
-                mRequestListener.onRequestError(TRACK_INFO_RESUILT, error);
+    protected Response.Listener<JSONObject> getTrackInfoListener(final RequestListener requestListener) {
+        return new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    Trackinfo ti = parseTrackInfo(response);
+                    Bundle bundle = new Bundle();
+                    bundle.putParcelable(Constants.Result.TRACKINFO, ti);
+                    requestListener.onResult(TRACK_INFO_RESUILT, bundle);
+                } catch (JSONException e) {
+                    RequestListener.RequestError error = new RequestListener.RequestError(e.getMessage());
+                    requestListener.onRequestError(TRACK_INFO_RESUILT, error);
+                }
             }
-        }
-    };
-    Response.ErrorListener vollerErrorListener = new Response.ErrorListener() {
-        @Override
-        public void onErrorResponse(VolleyError error) {
-            mRequestListener.onVolleyError(error);
-        }
-    };
+        };
+    }
+
+    protected Response.ErrorListener getErrorListener(final RequestListener requestListener){
+        return new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                requestListener.onVolleyError(error);
+            }
+        };
+    }
 
     protected Trackinfo parseTrackInfo(JSONObject data) throws JSONException {
         Log.d(LOG_TAG, "Result: " + data.toString());
         return Trackinfo.fromJSON(data);
     }
 
-    public JsonObjectRequest getTrackInfoRequest() {
-        String trackInfoPath = String.format(mContext.getString(R.string.trackinfo_uri), mVideoId);
-        String trackInfoUri = RutubeAPI.getUrl(mContext, trackInfoPath);
+    public JsonObjectRequest getTrackInfoRequest(Context context, RequestListener listener) {
+        String trackInfoPath = String.format(context.getString(R.string.trackinfo_uri), mVideoId);
+        String trackInfoUri = RutubeAPI.getUrl(context, trackInfoPath);
         JSONObject requestData = new JSONObject();
         JsonObjectRequest request = new JsonObjectRequest(trackInfoUri,
-                requestData, getTrackInfoListener, vollerErrorListener);
+                requestData, getTrackInfoListener(listener), getErrorListener(listener));
         request.setShouldCache(true);
         return request;
     }
 
+    public String getVideoId() {
+        return mVideoId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public Date getCreated() {
+        return mCreated;
+    }
+
+    public Uri getThumbnailUri() {
+        return mThumbnailUri;
+    }
+
+    public Author getAuthor() {
+        return mAuthor;
+    }
 }
