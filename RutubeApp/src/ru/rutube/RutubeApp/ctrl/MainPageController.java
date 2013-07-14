cedb18ad6fd4d346e2004997b86e92d6d2d0e404
tumb1er <zimbler@gmail.com>
2013-07-14 21:07:56 +0400

    Работа с табами на главной перенесена в контроллер (кроме авторизации).

---
+++ RutubeApp/src/ru/rutube/RutubeApp/ctrl/MainPageController.java
@@ -0,0 +1,113 @@
+package ru.rutube.RutubeApp.ctrl;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.HashMap;
+
+import ru.rutube.RutubeAPI.R;
+import ru.rutube.RutubeAPI.RutubeAPI;
+import ru.rutube.RutubeAPI.models.User;
+
+/**
+ * Created by tumbler on 14.07.13.
+ */
+public class MainPageController implements Parcelable {
+    private static final String TAB_EDITORS = "editors";
+    private static final String TAB_MY_VIDEO = "my_video";
+    private static final String TAB_SUBSCRIPTIONS = "subscription";
+    private HashMap<String, Uri> feedUriMap;
+
+    public interface MainPageView {
+        void addFeedTab(String name, String tag);
+        void selectTab(String tag);
+
+        void showFeedFragment(String tag, Uri feedUri);
+    }
+
+    private Context mContext = null;
+    private MainPageView mView = null;
+    private User mUser = null;
+    private boolean mAttached = false;
+    private String mSelectedTab;
+
+    public MainPageController() {
+        mSelectedTab = TAB_EDITORS;
+    }
+
+
+    public void attach(Context context, MainPageView view) {
+        assert mContext == null;
+        assert mView == null;
+        mContext = context;
+        mView = view;
+        mUser = User.load(context);
+        initFeedUriMap();
+        mAttached = true;
+    }
+
+    private void initFeedUriMap() {
+        feedUriMap = new HashMap<String, Uri>();
+        feedUriMap.put(TAB_EDITORS, Uri.parse(RutubeAPI.getUrl(mContext, R.string.editors_uri)));
+        feedUriMap.put(TAB_MY_VIDEO, Uri.parse(RutubeAPI.getUrl(mContext, R.string.my_video_uri)));
+        feedUriMap.put(TAB_SUBSCRIPTIONS, Uri.parse(RutubeAPI.getUrl(mContext, R.string.subscription_uri)));
+    }
+
+    public  void detach() {
+        mContext = null;
+        mView = null;
+        mAttached = false;
+    }
+
+    public void initTabs() {
+        assert mAttached;
+        mView.addFeedTab(mContext.getString(ru.rutube.RutubeFeed.R.id.editors_feed), TAB_EDITORS);
+        mView.addFeedTab(mContext.getString(ru.rutube.RutubeFeed.R.id.my_video), TAB_MY_VIDEO);
+        mView.addFeedTab(mContext.getString(ru.rutube.RutubeFeed.R.id.subscriptions), TAB_SUBSCRIPTIONS);
+        mView.selectTab(mSelectedTab);
+    }
+
+
+    public void onTabSelected(String tag) {
+        if (tag != TAB_EDITORS && !mUser.isAuthenticated()) {
+            showLoginDialog();
+            return;
+        }
+        Uri feedUri = feedUriMap.get(tag);
+        mView.showFeedFragment(tag, feedUri);
+    }
+
+    private void showLoginDialog() {
+
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+
+    }
+
+    public static MainPageController fromParcel(Parcel in) {
+        return new MainPageController();
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    public static final Parcelable.Creator<MainPageController> CREATOR
+            = new Parcelable.Creator<MainPageController>() {
+        public MainPageController createFromParcel(Parcel in) {
+            return MainPageController.fromParcel(in);
+        }
+
+        public MainPageController[] newArray(int size) {
+            return new MainPageController[size];
+        }
+    };
+
+}
--- RutubeApp/src/ru/rutube/RutubeApp/ui/StartActivity.java
+++ RutubeApp/src/ru/rutube/RutubeApp/ui/StartActivity.java
@@ -1,6 +1,7 @@
 package ru.rutube.RutubeApp.ui;
 
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.content.Intent;
@@ -9,134 +10,106 @@ import android.os.Bundle;
 import android.util.Log;
 
 import ru.rutube.RutubeAPI.models.Constants;
-import ru.rutube.RutubeAPI.models.User;
 import ru.rutube.RutubeApp.R;
+import ru.rutube.RutubeApp.ctrl.MainPageController;
 import ru.rutube.RutubeFeed.ui.FeedActivity;
 import ru.rutube.RutubeFeed.ui.FeedFragment;
 
 import java.util.HashMap;
 
-public class StartActivity extends FeedActivity implements LoginFragment.LoginListener {
+public class StartActivity extends Activity implements MainPageController.MainPageView, ActionBar.TabListener {
     private static final String LOG_TAG = StartActivity.class.getName();
-    protected static final int LOGIN_REQUEST_CODE = 1;
+    private static final String CONTROLLER = "controller";
+
+    private MainPageController mController;
+    private HashMap<String, ActionBar.Tab> mTabMap = new HashMap<String, ActionBar.Tab>();
+    private HashMap<String, Fragment> mFragmentMap = new HashMap<String, Fragment>();
+    private FragmentTransaction mFragmentTransaction;
+    private Fragment mCurrentFragment;
+
     private static final String SELECTED_TAB = "SelectedTab";
-    private static final String TAB_EDITORS = "editors";
-    private static final String TAB_MY_VIDEO = "my_video";
-    private static final String TAB_SUBSCRIPTIONS = "subscription";
+    protected static final int LOGIN_REQUEST_CODE = 1;
     private static final String TAB_LOGIN = "login";
     private String selectedTab;
-    private HashMap<String, ActionBar.Tab> tabs = new HashMap<String, ActionBar.Tab>();
-    private HashMap<String, Fragment> fragments = new HashMap<String, Fragment>();
-    private User mUser;
-
-    @Override
-    public void setContentView(int layoutResId) {
-        super.setContentView(R.layout.start_activity);
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        //layoutRedId = R.layout.start_activity;
-        mUser = new User(User.getToken(this));
-        getIntent().setData(buildFeedUri(R.string.editors_uri));
-        super.onCreate(savedInstanceState);
         if (savedInstanceState != null)
-            selectedTab = savedInstanceState.getString(SELECTED_TAB);
+            mController = savedInstanceState.getParcelable(CONTROLLER);
         else
-            selectedTab = TAB_EDITORS;
-//        setContentView(ru.rutube.RutubeFeed.R.layout.feed_activity);
-        //processIntentUri();
+            mController = new MainPageController();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_activity);
+        mController.attach(this, this);
         initTabs();
     }
 
-    ActionBar.TabListener tabListener = new ActionBar.TabListener() {
-
-        @Override
-        public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-            Object tag = tab.getTag();
-            Log.d(LOG_TAG, "Selected tab: " + selectedTab);
-            Fragment f = getFragmentManager().findFragmentByTag(selectedTab);
-            Fragment fragment = selectFeed(tag);
-            Log.d(LOG_TAG, "Changed to tab: " + selectedTab);
-            if (fragment != null) {
-                if (f != null) {
-                    ft.remove(f);
-
-                    ft.add(R.id.feed_fragment_container, fragment, String.valueOf(tag));
-                    Log.d(LOG_TAG, "Fragment replaced");
-
-                } else {
-                    ft.add(R.id.feed_fragment_container, fragment, String.valueOf(tag));
-                    Log.d(LOG_TAG, "Fragment added");
-                }
-            }
-        }
-
-        @Override
-        public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-            //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        @Override
-        public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
-            //To change body of implemented methods use File | Settings | File Templates.
-        }
-    };
-
-    private Fragment selectFeed(Object tag) {
-        Bundle bundle = new Bundle();
-        int id = R.string.editors_uri;
-        selectedTab = (String)tag;
-        if (tag.equals(TAB_EDITORS)) {
-            id = R.string.editors_uri;
-        } else if (!mUser.isAuthenticated()) {
-
-            selectedTab = TAB_LOGIN;
-            Fragment loginFragment = fragments.get(selectedTab);
-            if (loginFragment == null) {
-             loginFragment = new LoginFragment();
-                fragments.put(selectedTab, loginFragment);
-            }
-            return loginFragment;
-        }
-        if (tag.equals(TAB_MY_VIDEO)) {
-            id = R.string.my_video_uri;
-        }
-        if (tag.equals(TAB_SUBSCRIPTIONS)) {
-            id = R.string.subscription_uri;
-        }
-        bundle.putParcelable(Constants.Params.FEED_URI, buildFeedUri(id));
-        Fragment fragment = fragments.get(selectedTab);
-        if (fragment == null) {
-            Log.d(LOG_TAG, "Creting new fragment");
-            fragment = new FeedFragment();
-            fragment.setArguments(bundle);
-            fragments.put(selectedTab, fragment);
-        }
-        else {
-            Log.d(LOG_TAG, "Got fragment from cache");
-        }
-        return fragment;
-
+    public void selectTab(String tag) {
+        getActionBar().selectTab(mTabMap.get(selectedTab));
     }
 
     private void initTabs() {
         Log.d(LOG_TAG, "initTabs");
         getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        addTab(getString(ru.rutube.RutubeFeed.R.id.editors_feed), TAB_EDITORS);
-        addTab(getString(ru.rutube.RutubeFeed.R.id.my_video), TAB_MY_VIDEO);
-        addTab(getString(ru.rutube.RutubeFeed.R.id.subscriptions), TAB_SUBSCRIPTIONS);
-        getActionBar().selectTab(tabs.get(selectedTab));
+        mController.initTabs();
+        Log.d(LOG_TAG, "initTabs done");
+
     }
 
-    private ActionBar.Tab addTab(String title, String tag) {
-        ActionBar.Tab tab = getActionBar().newTab();
+    public void addFeedTab(String title, String tag) {
+        ActionBar actionBar = getActionBar();
+        assert actionBar != null;
+        ActionBar.Tab tab = actionBar.newTab();
         tab.setText(title);
-        tab.setTabListener(tabListener);
+        tab.setTabListener(this);
         tab.setTag(tag);
-        getActionBar().addTab(tab);
-        tabs.put(tag, tab);
-        return tab;
+        actionBar.addTab(tab);
+        mTabMap.put(tag, tab);
+    }
+
+    @Override
+    public void showFeedFragment(String tag, Uri feedUri) {
+        Log.d(LOG_TAG, "showFeedFragment " + tag);
+        Fragment fragment = mFragmentMap.get(tag);
+        Boolean isNewFragment = false;
+        if (fragment == null) {
+            Log.d(LOG_TAG, "creating fragment for " + feedUri.toString());
+            fragment = createFeedFragment(feedUri);
+            isNewFragment = true;
+            mFragmentMap.put(tag, fragment);
+        }
+
+        if (mFragmentTransaction != null){
+            Log.d(LOG_TAG, "ft not null");
+            if (mCurrentFragment != null){
+                Log.d(LOG_TAG, "hiding");
+                mFragmentTransaction.hide(mCurrentFragment);
+            }
+            if (isNewFragment)
+            {
+                mFragmentTransaction.add(R.id.feed_fragment_container, fragment);
+                Log.d(LOG_TAG, "Added fragment");
+            }
+            else {
+                mFragmentTransaction.show(fragment);
+                Log.d(LOG_TAG, "Showed fragment");
+            }
+        }
+        else {
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.replace(R.id.feed_fragment_container, fragment, tag);
+            ft.commit();
+        }
+        mCurrentFragment = fragment;
+        Log.d(LOG_TAG, "showFeedFragment done");
+    }
+
+    private Fragment createFeedFragment(Uri feedUri) {
+        Fragment fragment = new FeedFragment();
+        Bundle args = new Bundle();
+        args.putParcelable(Constants.Params.FEED_URI, feedUri);
+        fragment.setArguments(args);
+        return fragment;
     }
 
     private Uri buildFeedUri(int resourceId) {
@@ -156,7 +129,8 @@ public class StartActivity extends FeedActivity implements LoginFragment.LoginLi
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         Log.d(LOG_TAG, "onActivityResult");
         if (requestCode == LOGIN_REQUEST_CODE && resultCode == RESULT_OK) {
-            processCurrentTab();
+            //processCurrentTab();
+            // TODO: обработка процесса авторизации
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
@@ -168,20 +142,28 @@ public class StartActivity extends FeedActivity implements LoginFragment.LoginLi
         outState.putString(SELECTED_TAB, selectedTab);
     }
 
-    private void processCurrentTab() {
-        Object tag = getActionBar().getSelectedTab().getTag();
-        Log.d(LOG_TAG, "processCurrentTab " + String.valueOf(tag));
-        Fragment fragment = selectFeed(tag);
-        if (fragment != null) {
-            FragmentTransaction ft = getFragmentManager().beginTransaction();
-            ft.replace(R.id.feed_fragment_container, fragment, tag.toString());
-            ft.commit();
-        }
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        mFragmentTransaction = fragmentTransaction;
+        String tag = (String)tab.getTag();
+        mController.onTabSelected(tag);
+        mFragmentTransaction = null;
+
     }
 
     @Override
-    public void onLoginResult(int result) {
-        if (result == RESULT_OK)
-            processCurrentTab();
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
     }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
+
+//    @Override
+//    public void onLoginResult(int result) {
+//        if (result == RESULT_OK)
+//            processCurrentTab();
+//    }
 }
