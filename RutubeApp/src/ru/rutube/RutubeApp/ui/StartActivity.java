500c2116e54e2b2b11080289198080d5e46c98ef
Sergey Tikhonov <zimbler@gmail.com>
2013-07-15 10:52:59 +0400

    Комменты и прическа StartActivity

--- RutubeApp/src/ru/rutube/RutubeApp/ui/StartActivity.java
+++ RutubeApp/src/ru/rutube/RutubeApp/ui/StartActivity.java
@@ -20,17 +20,13 @@ import java.util.HashMap;
 public class StartActivity extends Activity implements MainPageController.MainPageView, ActionBar.TabListener {
     private static final String LOG_TAG = StartActivity.class.getName();
     private static final String CONTROLLER = "controller";
+    private static final int LOGIN_REQUEST_CODE = 1;
 
     private MainPageController mController;
     private HashMap<String, ActionBar.Tab> mTabMap = new HashMap<String, ActionBar.Tab>();
     private HashMap<String, Fragment> mFragmentMap = new HashMap<String, Fragment>();
     private FragmentTransaction mFragmentTransaction;
     private Fragment mCurrentFragment;
-
-    private static final String SELECTED_TAB = "SelectedTab";
-    protected static final int LOGIN_REQUEST_CODE = 1;
-    private static final String TAB_LOGIN = "login";
-    private String selectedTab;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -44,104 +40,12 @@ public class StartActivity extends Activity implements MainPageController.MainPa
         initTabs();
     }
 
-    public void selectTab(String tag) {
-        getActionBar().selectTab(mTabMap.get(selectedTab));
-    }
-
-    private void initTabs() {
-        Log.d(LOG_TAG, "initTabs");
-        getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        mController.initTabs();
-        Log.d(LOG_TAG, "initTabs done");
-
-    }
-
-    public void addFeedTab(String title, String tag) {
-        ActionBar actionBar = getActionBar();
-        assert actionBar != null;
-        ActionBar.Tab tab = actionBar.newTab();
-        tab.setText(title);
-        tab.setTabListener(this);
-        tab.setTag(tag);
-        actionBar.addTab(tab);
-        mTabMap.put(tag, tab);
-    }
-
-    @Override
-    public void showFeedFragment(String tag, Uri feedUri) {
-        Log.d(LOG_TAG, "showFeedFragment " + tag);
-        Fragment fragment = mFragmentMap.get(tag);
-        Boolean isNewFragment = false;
-        if (fragment == null) {
-            Log.d(LOG_TAG, "creating fragment for " + feedUri.toString());
-            fragment = createFeedFragment(feedUri);
-            isNewFragment = true;
-            mFragmentMap.put(tag, fragment);
-        }
-
-        if (mFragmentTransaction != null){
-            Log.d(LOG_TAG, "ft not null");
-            if (mCurrentFragment != null){
-                Log.d(LOG_TAG, "hiding");
-                mFragmentTransaction.hide(mCurrentFragment);
-            }
-            if (isNewFragment)
-            {
-                mFragmentTransaction.add(R.id.feed_fragment_container, fragment);
-                Log.d(LOG_TAG, "Added fragment");
-            }
-            else {
-                mFragmentTransaction.show(fragment);
-                Log.d(LOG_TAG, "Showed fragment");
-            }
-        }
-        else {
-            FragmentTransaction ft = getFragmentManager().beginTransaction();
-            ft.replace(R.id.feed_fragment_container, fragment, tag);
-            ft.commit();
-        }
-        mCurrentFragment = fragment;
-        Log.d(LOG_TAG, "showFeedFragment done");
-    }
-
-    private Fragment createFeedFragment(Uri feedUri) {
-        Fragment fragment = new FeedFragment();
-        Bundle args = new Bundle();
-        args.putParcelable(Constants.Params.FEED_URI, feedUri);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    private Uri buildFeedUri(int resourceId) {
-        Uri.Builder builder = Uri.parse(getString(R.string.base_uri)).buildUpon();
-        builder.appendEncodedPath(getString(resourceId));
-        return builder.build();
-    }
-
-    private void processIntentUri() {
-        Intent intent = new Intent(this, FeedActivity.class);
-        intent.setData(buildFeedUri(R.string.editors_uri));
-        startActivity(intent);
-        finish();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.d(LOG_TAG, "onActivityResult");
-        if (requestCode == LOGIN_REQUEST_CODE && resultCode == RESULT_OK) {
-            //processCurrentTab();
-            // TODO: обработка процесса авторизации
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString(SELECTED_TAB, selectedTab);
-    }
-
+    /**
+     * Проксирует обработку выбора вкладки в контроллер, попутно запоминая текущую транзакцию
+     * фрагмента для использования в обратном вызове
+     * @param tab тег вкладки
+     * @param fragmentTransaction
+     */
     @Override
     public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
         mFragmentTransaction = fragmentTransaction;
@@ -159,6 +63,112 @@ public class StartActivity extends Activity implements MainPageController.MainPa
     @Override
     public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
 
+    }
+
+    /**
+     * Делает активной вкладку с тегом
+     * @param tag
+     */
+    public void selectTab(String tag) {
+        getActionBar().selectTab(mTabMap.get(tag));
+    }
+
+    /**
+     * Добавляет новую вкладку в таб-навигацию
+     * @param title
+     * @param tag
+     */
+    public void addFeedTab(String title, String tag) {
+        ActionBar actionBar = getActionBar();
+        assert actionBar != null;
+        ActionBar.Tab tab = actionBar.newTab();
+        tab.setText(title);
+        tab.setTabListener(this);
+        tab.setTag(tag);
+        actionBar.addTab(tab);
+        mTabMap.put(tag, tab);
+    }
+
+    /**
+     * Делает активным фрагмент ленты с определенным тегом
+     * @param tag тег фрагмента
+     * @param feedUri uri ленты
+     */
+    public void showFeedFragment(String tag, Uri feedUri) {
+        // ищем фрагмент в локальном кэше
+        Fragment fragment = mFragmentMap.get(tag);
+        Boolean isNewFragment = false;
+        // не нашли, конструируем новый фрагмент с feedUri
+        if (fragment == null) {
+            fragment = createFeedFragment(feedUri);
+            // добавляем в кэш
+            isNewFragment = true;
+            mFragmentMap.put(tag, fragment);
+        }
+
+        // Транзакция может уже быть открыта, если метод вызывается в обработчике таб-навигации,
+        if (mFragmentTransaction != null){
+            // Скрываем старый фрагмент
+            if (mCurrentFragment != null){
+                mFragmentTransaction.hide(mCurrentFragment);
+            }
+            // Добавляем или показываем новый фрагмент
+            if (isNewFragment)
+            {
+                mFragmentTransaction.add(R.id.feed_fragment_container, fragment);
+            }
+            else {
+                mFragmentTransaction.show(fragment);
+            }
+        }
+        else {
+            // Заменяем заглушку на новый фрагмент
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.replace(R.id.feed_fragment_container, fragment, tag);
+            ft.commit();
+        }
+        mCurrentFragment = fragment;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable(CONTROLLER, mController);
+    }
+
+    /**
+     * Настраивает таб-навигацию
+     */
+    private void initTabs() {
+        Log.d(LOG_TAG, "initTabs");
+        getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        mController.initTabs();
+        Log.d(LOG_TAG, "initTabs done");
+
+    }
+
+    /**
+     * Конструирует новый фрагмент с лентой
+     * @param feedUri uri ленты
+     * @return готовый к использованию фрагмент ленты
+     */
+    private Fragment createFeedFragment(Uri feedUri) {
+        Fragment fragment = new FeedFragment();
+        Bundle args = new Bundle();
+        args.putParcelable(Constants.Params.FEED_URI, feedUri);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(LOG_TAG, "onActivityResult");
+        if (requestCode == LOGIN_REQUEST_CODE && resultCode == RESULT_OK) {
+            //processCurrentTab();
+            // TODO: обработка процесса авторизации
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
 //    @Override
