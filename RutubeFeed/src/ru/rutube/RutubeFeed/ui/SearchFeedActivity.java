2bcd09d408d9174ebecd6b6ccb4e36df5d193b9c
Sergey Tikhonov <zimbler@gmail.com>
2013-07-30 09:48:00 +0400

    Подсказки для поиска

--- RutubeFeed/src/ru/rutube/RutubeFeed/ui/SearchFeedActivity.java
+++ RutubeFeed/src/ru/rutube/RutubeFeed/ui/SearchFeedActivity.java
@@ -1,19 +1,15 @@
 package ru.rutube.RutubeFeed.ui;
 
 import android.app.SearchManager;
-import android.content.ContentValues;
 import android.content.Intent;
-import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.SearchRecentSuggestions;
 import android.util.Log;
-
-import java.util.Date;
 
 import ru.rutube.RutubeAPI.R;
 import ru.rutube.RutubeAPI.RutubeAPI;
-import ru.rutube.RutubeAPI.content.FeedContract;
-import ru.rutube.RutubeAPI.models.FeedItem;
+import ru.rutube.RutubeFeed.content.SearchSuggestProvider;
 
 /**
  * Created by tumbler on 29.07.13.
@@ -27,6 +23,10 @@ public class SearchFeedActivity extends FeedActivity {
         Intent intent = getIntent();
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
+            SearchRecentSuggestions searchRecentSuggestions = new SearchRecentSuggestions(this,
+                    SearchSuggestProvider.AUTHORITY,
+                    SearchSuggestProvider.MODE);
+            searchRecentSuggestions.saveRecentQuery(query, null);
             Uri feedUri = getSearchFeedUri(query);
             intent = new Intent();
             Log.d(LOG_TAG, "Computed URI: " + feedUri.toString());
