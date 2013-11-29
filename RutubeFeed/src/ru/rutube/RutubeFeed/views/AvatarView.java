ee459f5a7aa9555583f14e8a9b3033c11fad9f61
Sergey Tikhonov <zimbler@gmail.com>
2013-11-29 12:18:05 +0400

    small styles fixes and inEditMode for AvatarView

--- RutubeFeed/src/ru/rutube/RutubeFeed/views/AvatarView.java
+++ RutubeFeed/src/ru/rutube/RutubeFeed/views/AvatarView.java
@@ -11,26 +11,35 @@ import ru.rutube.RutubeFeed.R;
  * Created by tumbler on 06.11.13.
  */
 public class AvatarView extends NetworkImageView {
-    public static int[] sStubs = {R.drawable._thumb_default_00,
-            R.drawable._thumb_default_01,
-            R.drawable._thumb_default_02,
-            R.drawable._thumb_default_03,
-            R.drawable._thumb_default_04,
-            R.drawable._thumb_default_05,
-            R.drawable._thumb_default_06,
-            R.drawable._thumb_default_07,
-    };
+    public int[] sStubs;
 
     public AvatarView(Context context) {
         super(context);
+        initStubs();
+    }
+
+    private void initStubs() {
+        if (!isInEditMode()) {
+            sStubs = new int[]{R.drawable._thumb_default_00,
+                     R.drawable._thumb_default_01,
+                     R.drawable._thumb_default_02,
+                     R.drawable._thumb_default_03,
+                     R.drawable._thumb_default_04,
+                     R.drawable._thumb_default_05,
+                     R.drawable._thumb_default_06,
+                     R.drawable._thumb_default_07,
+             };
+        }
     }
 
     public AvatarView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initStubs();
     }
 
     public AvatarView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        initStubs();
     }
 
     public void setDefaultImageRes(int author_id) {
