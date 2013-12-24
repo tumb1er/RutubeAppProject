3781dc89b74139b4da3bc1a5b95aee323cd697b1
tumb1er <zimbler@gmail.com>
2013-12-24 13:26:02 +0400

    close #96 don't reload same images int ThumbnailView, memory cache flag

--- RutubeAPI/src/ru/rutube/RutubeAPI/tools/MemDiskBitmapCache.java
+++ RutubeAPI/src/ru/rutube/RutubeAPI/tools/MemDiskBitmapCache.java
@@ -17,23 +17,31 @@ import ru.rutube.RutubeAPI.RutubeApp;
 public class MemDiskBitmapCache implements ImageLoader.ImageCache {
     private static final boolean D = BuildConfig.DEBUG;
     private static final String LOG_TAG = MemDiskBitmapCache.class.getName();
-    // private BitmapLruCache mMemoryCache;
+    private static final boolean MEMORY_ENABLED = true;
+    private BitmapLruCache mMemoryCache;
     private DiskLruImageCache mDiskCache;
 
     public MemDiskBitmapCache(String cacheDir) {
         mDiskCache = new DiskBitmapCache(RutubeApp.getContext(), cacheDir, 20*1024*1024,
                 Bitmap.CompressFormat.PNG, 100);
-        // mMemoryCache = new BitmapLruCache((int)(Runtime.getRuntime().maxMemory() / 1024) / 16);
+        if (MEMORY_ENABLED)
+            mMemoryCache = new BitmapLruCache((int)(Runtime.getRuntime().maxMemory() / 1024) / 16);
     }
 
     @Override
     public Bitmap getBitmap(String url) {
         String key = getKey(url);
-//        Bitmap result = mMemoryCache.getBitmap(key);
-//        if (result != null)
-//            return result;
+        Bitmap result;
         if (D) Log.d(LOG_TAG, "Start getting bitmap " + key);
-        Bitmap result = mDiskCache.getBitmap(key);
+        if (MEMORY_ENABLED)
+        {
+            result = mMemoryCache.getBitmap(key);
+            if (result != null)
+                return result;
+        }
+        result = mDiskCache.getBitmap(key);
+        if (result != null && MEMORY_ENABLED)
+            mMemoryCache.putBitmap(key, result);
         if (D) Log.d(LOG_TAG, "End getting bitmap " + key);
         return result;
     }
@@ -41,15 +49,22 @@ public class MemDiskBitmapCache implements ImageLoader.ImageCache {
     protected String getKey(String url) {
         int keyStart = url.lastIndexOf('/');
         int keyEnd = url.lastIndexOf('.');
-        return url.substring(keyStart + 1, keyEnd);
+        int sizePos = url.lastIndexOf("size");
+        if (sizePos < 0)
+            return url.substring(keyStart + 1, keyEnd) + "n";
+        else
+            return url.substring(keyStart + 1, keyEnd) + url.substring(sizePos + 5, sizePos + 6);
     }
 
     @Override
     public void putBitmap(String url, Bitmap bitmap) {
         String key = getKey(url);
-        // mMemoryCache.putBitmap(key, bitmap);
+        if (D) Log.d(LOG_TAG, "start saving bitmap " + key);
+        if (MEMORY_ENABLED)
+            mMemoryCache.putBitmap(key, bitmap);
         SaveToDiskCacheTask task = new SaveToDiskCacheTask(bitmap);
         task.execute(key);
+        if (D) Log.d(LOG_TAG, "End saving bitmap " + key);
     }
 
     protected class SaveToDiskCacheTask extends AsyncTask<String, Void, Void> {
@@ -62,11 +77,9 @@ public class MemDiskBitmapCache implements ImageLoader.ImageCache {
         @Override
         protected Void doInBackground(String... keys) {
             String key = keys[0];
-            if (D) Log.d(LOG_TAG, "start saving bitmap " + key);
             try {
                 mDiskCache.putBitmap(key, bitmapReference.get());
             } catch (NullPointerException e) {}
-            if (D) Log.d(LOG_TAG, "End saving bitmap " + key);
             return null;
 
         }
--- RutubeFeed/src/ru/rutube/RutubeFeed/views/ThumbnailView.java
+++ RutubeFeed/src/ru/rutube/RutubeFeed/views/ThumbnailView.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.util.AttributeSet;
 import android.util.Log;
 
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.NetworkImageView;
 
 import ru.rutube.RutubeAPI.RutubeApp;
@@ -18,6 +19,7 @@ import ru.rutube.RutubeFeed.helpers.TopRoundCornerBitmapProcessor;
  */
 public class ThumbnailView extends NetworkImageView {
     private static double ASPECT = 0.668;
+    private String mImageUrl = null;
     private static final BitmapProcessor sBitmapProcessor = new CropBitmapProcessor(ASPECT);
     public ThumbnailView(Context context) {
         super(context);
@@ -35,6 +37,14 @@ public class ThumbnailView extends NetworkImageView {
 //    public void setImageBitmap(Bitmap bm) {
 //        super.setImageBitmap(sBitmapProcessor.process(bm, this));
 //    }
+
+
+    @Override
+    public void setImageUrl(String url, ImageLoader imageLoader) {
+        if (mImageUrl != null && mImageUrl.equals(url))
+            return;
+        super.setImageUrl(url, imageLoader);
+    }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
